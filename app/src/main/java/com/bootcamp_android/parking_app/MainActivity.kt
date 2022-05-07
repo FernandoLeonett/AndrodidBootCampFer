92dd91dcf339b7a608da3022d511fa836f8f64aa
Fernando <fernandoleonett@gmail.com>
2022-05-06 23:04:03 -0300

    Update:Package Name,AppName getStart: RecycleView lot
    Signed-off-by: Fernando <fernandoleonett@gmail.com>

--- app/src/main/java/com/bootcamp_android/parking_app/MainActivity.kt
+++ app/src/main/java/com/bootcamp_android/parking_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.bootcamp_android.parking_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }

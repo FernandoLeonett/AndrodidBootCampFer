e52bef015fa281fc9df1aae019e6d9f552c85a4c
Fernando <fernandoleonett@gmail.com>
2022-05-10 02:22:42 -0300

    feature: changed recycled layout to constraint layout
    Signed-off-by: Fernando <fernandoleonett@gmail.com>

--- app/src/main/java/com/bootcamp_android/parking_app/adapter/Provider.kt
+++ app/src/main/java/com/bootcamp_android/parking_app/adapter/Provider.kt
@@ -6,7 +6,13 @@ class Provider {
 
 
     companion object {
-        val lots = listOf(
+        val lots = list()
+
+        private fun list() = listOf(
+
+
+
+            Lot("88","Miercoles, 25 May,2022","8:30 pm",true),
 
             Lot("1","Monday, 25 May,2022","8:30 pm",false),
             Lot("1","Monday, 25 May,2022","8:30 pm",false),
@@ -22,10 +28,10 @@ class Provider {
             Lot("2","Monday, 25 May,2022","8:30 pm",true),
             Lot("3","Monday, 25 May,2022","8:30 pm",false),
             Lot("4","Monday, 25 May,2022","8:30 pm",false),
-            Lot("5","Monday, 25 May,2022","8:30 pm", false),
+            Lot("5","Monday, 25 May,2022","8:30 pm",false),
             Lot("6","Monday, 25 May,2022","8:30 pm",false),
-            Lot("7","Monday, 25 May,2022","8:30 pm",false),
-            Lot("7","Monday, 25 May,2022","8:30 pm", true)
+            Lot("99","Monday, 25 May,2022","8:30 pm",false),
+
         )
 
     }

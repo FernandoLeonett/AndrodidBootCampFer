6760922e1aba320ec23778e91fc468966c70cf94
Fernando <fernandoleonett@gmail.com>
2022-05-13 23:17:34 -0300

    fix: lotlist fragment working but no working recycle
    Signed-off-by: Fernando <fernandoleonett@gmail.com>

--- app/src/main/java/com/bootcamp_android/parking_app/fragments/LotList.kt
+++ app/src/main/java/com/bootcamp_android/parking_app/fragments/LotList.kt
@@ -4,30 +4,38 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bootcamp_android.parking_app.adapter.Adapter
+import com.bootcamp_android.parking_app.adapter.Lot
 import com.bootcamp_android.parking_app.adapter.Provider
 import com.bootcamp_android.parking_app.databinding.FragmentLotsBinding
 
 class LotList : Fragment() {
-    private lateinit var _binding: FragmentLotsBinding
+    private var _binding: FragmentLotsBinding? = null
+    private val binding: FragmentLotsBinding get() = _binding!!
     private lateinit var dataAdapter: Adapter
-    private val binding: FragmentLotsBinding = _binding
+
+
     override fun onCreateView(
         inflater: LayoutInflater,container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentLotsBinding.inflate(inflater)
         return binding.root
     }
 
     override fun onViewCreated(itemView: View,savedInstanceState: Bundle?) {
-        binding.lotList
-            .apply {
-                layoutManager = androidx.recyclerview.widget.LinearLayoutManager(activity)
-                hasFixedSize()
-                this.adapter = dataAdapter
-                dataAdapter.setData(Provider.lots)
-            }
+
+
+        Toast.makeText(activity,"PASÉ POR AQUÍ",Toast.LENGTH_SHORT).show(); //Correcto
+//        binding.recyclerLotList
+//            .apply {
+//                layoutManager = LinearLayoutManager(activity)
+//
+//                adapter = dataAdapter
+//                dataAdapter.setData(Provider.lots)
+//            }
     }
 }

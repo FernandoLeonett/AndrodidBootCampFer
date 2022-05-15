fd6872242030a90c232aa9b9a8a08359523ae8e9
Fernando <fernandoleonett@gmail.com>
2022-05-15 01:19:08 -0300

    fix: its working fab, fragments, events <fernandoleonett@gmail.com>
    Signed-off-by: Fernando <fernandoleonett@gmail.com>

--- app/src/main/java/com/bootcamp_android/parking_app/adapter/Adapter.kt
+++ app/src/main/java/com/bootcamp_android/parking_app/adapter/Adapter.kt
@@ -1,19 +1,15 @@
 package com.bootcamp_android.parking_app.adapter
 
-import android.text.method.TextKeyListener.clear
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bootcamp_android.parking_app.R
-import java.util.Collections.addAll
 
 class Adapter(private val adapterData: List<DataModel>) : RecyclerView.Adapter<DataViewHolder>() {
     //    private val adapterData = mutableListOf<DataModel>()
-
-
     override fun onCreateViewHolder(parent: ViewGroup,viewType: Int): DataViewHolder {
 // here i cannot get one view holder, that`s the reason i assign an custom int to the view type in getItemViewType
-        val layout = when (viewType) {
+        val layout = when(viewType) {
             TYPE_LOT -> R.layout.fragment_lot
             TYPE_RESERVATION -> R.layout.fragment_reservation
             else -> throw IllegalArgumentException("Invalid type")
@@ -35,21 +31,15 @@ class Adapter(private val adapterData: List<DataModel>) : RecyclerView.Adapter<D
 
     //ask for the type of the view and return it
     override fun getItemViewType(position: Int): Int {
-        return when (adapterData[position]) {
+        return when(adapterData[position]) {
             is Lot -> TYPE_LOT
             else -> TYPE_RESERVATION
         }
     }
 
-//    fun setData(data: List<DataModel>) {
-//        adapterData.apply {
-//            clear()
-//            addAll(data)
-//        }
-//    }
-
     // statics methods for define view types i assign and int for each type so i can compare later
     companion object {
+
         private const val TYPE_LOT = 0
         private const val TYPE_RESERVATION = 1
     }

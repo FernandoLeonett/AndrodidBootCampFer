211d9f9a9b2fceafbf9bf3b0144a4f5e9ce1a83b
Fernando <fernandoleonett@gmail.com>
2022-05-15 00:46:50 -0300

    fix: added events on buttons screens reservations and lots working <fernandoleonett@gmail.com>
    Signed-off-by: Fernando <fernandoleonett@gmail.com>

--- app/src/main/java/com/bootcamp_android/parking_app/MainActivity.kt
+++ app/src/main/java/com/bootcamp_android/parking_app/MainActivity.kt
@@ -2,19 +2,11 @@ package com.bootcamp_android.parking_app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
-import com.bootcamp_android.parking_app.adapter.Adapter
-import com.bootcamp_android.parking_app.adapter.Provider
 
 class MainActivity : AppCompatActivity() {
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
-
-
 }
--- app/src/main/java/com/bootcamp_android/parking_app/adapter/DataViewHolder.kt
+++ app/src/main/java/com/bootcamp_android/parking_app/adapter/DataViewHolder.kt
@@ -4,13 +4,20 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.bootcamp_android.parking_app.databinding.FragmentLotBinding
 import com.bootcamp_android.parking_app.databinding.FragmentReservationBinding
+import com.google.android.material.snackbar.Snackbar
 
 class DataViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+
     private fun bindLot(lot: Lot) {
         val binding = FragmentLotBinding.bind(view)
         binding.textIdLot.text = lot.id
-        binding.textDateLot.text = if (!lot.available) lot.date else "free"
+        binding.textDateLot.text = if(!lot.available) lot.date else "free"
         binding.textHourLot.text = lot.hour
+        view.setOnClickListener { view ->
+            Snackbar.make(view,"Here's a Snackbar of lot ${lot.id}",Snackbar.LENGTH_LONG)
+                .setAction("Action",null)
+                .show()
+        }
     }
 
     private fun bindReservation(reservation: Reservation) {
@@ -19,10 +26,19 @@ class DataViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         binding.textHourStart.text = reservation.startHour
         binding.textEndHour.text = reservation.endHour
         binding.textDateEnd.text = reservation.endDate
+        binding.btnDeleteReservation.setOnClickListener {
+            Snackbar.make(
+                view,
+                "Here's a Snackbar reserva ${reservation.endDate}",
+                Snackbar.LENGTH_LONG
+            )
+                .setAction("Action",null)
+                .show()
+        }
     }
 
     fun binding(dataModel: DataModel) {
-        when (dataModel) {
+        when(dataModel) {
             is Lot -> bindLot(dataModel)
             is Reservation -> bindReservation(dataModel)
         }

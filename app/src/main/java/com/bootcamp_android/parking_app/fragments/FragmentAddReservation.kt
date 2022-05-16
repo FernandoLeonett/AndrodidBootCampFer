533107ce47902a86e04eb1aea77dd5466b87c090
Fernando <fernandoleonett@gmail.com>
2022-05-16 18:20:20 -0300

    feature: starting spinner
    Signed-off-by: Fernando <fernandoleonett@gmail.com>

--- app/src/main/java/com/bootcamp_android/parking_app/fragments/FragmentAddReservation.kt
+++ app/src/main/java/com/bootcamp_android/parking_app/fragments/FragmentAddReservation.kt
@@ -3,7 +3,6 @@ package com.bootcamp_android.parking_app.fragments
 import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
-import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import com.bootcamp_android.parking_app.R
 import com.bootcamp_android.parking_app.databinding.FragmentAddReservationBinding
@@ -13,19 +12,14 @@ class FragmentAddReservation : Fragment(R.layout.fragment_add_reservation) {
     override fun onViewCreated(view: View,savedInstanceState: Bundle?) {
         super.onViewCreated(view,savedInstanceState)
         val binding = FragmentAddReservationBinding.bind(view)
-        val spinner: Spinner = binding.planetsSpinner
-// Create an ArrayAdapter using the string array and a default spinner layout
-        activity?.let {
-            ArrayAdapter.createFromResource(
-                it,
-                R.array.planets_array,
-                android.R.layout.simple_spinner_item
-            ).also { adapter ->
-                // Specify the layout to use when the list of choices appears
-                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-                // Apply the adapter to the spinner
-                binding.planetsSpinner.adapter = adapter
-            }
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.slots_options_spinner,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            binding.lotsOptionsSpinner.adapter = adapter
         }
     }
 }

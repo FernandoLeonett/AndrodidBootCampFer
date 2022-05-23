f29312c5b3a656cc9890eaf66e0f68695d07bd72
Fernando <fernandoleonett@gmail.com>
2022-05-23 14:33:48 -0300

    back button arrow
    working ahi no mas
    
    Signed-off-by: Fernando <fernandoleonett@gmail.com>

--- app/src/main/java/com/bootcamp_android/parking_app/view/MainActivity.kt
+++ app/src/main/java/com/bootcamp_android/parking_app/view/MainActivity.kt
@@ -2,15 +2,36 @@ package com.bootcamp_android.parking_app.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.bootcamp_android.parking_app.R
 import com.bootcamp_android.parking_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
+        setupNavigation()
         setContentView(binding.root)
+    }
+
+    private fun setupNavigation() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration.Builder(navController.graph).build()
+
+        setupActionBarWithNavController(navController,appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
